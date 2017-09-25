c55cf96a99dca51fb0d0128a1e5d7fcf583e0b5a
sergysnchez <sergio.sanchezcolilla@altran.com>
2017-09-25 17:21:11 +0200

    Fix issues

--- app/components/movie-view.js
+++ app/components/movie-view.js
@@ -1,9 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    response: "",
     actions: {
         getMovie(titulo) {
+          var self = this;
           Ember.$.get('http://localhost:8080/getMovie?name=' + titulo).then(function(data){
+            self.set('response',data);
           });
         }
       }
