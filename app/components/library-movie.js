4cb1ce611288e549493a15adc2c20c2f64d730a4
sergysnchez <sergio.sanchezcolilla@altran.com>
2017-10-03 12:20:36 +0200

    Loading spinner

--- app/components/library-movie.js
+++ app/components/library-movie.js
@@ -4,24 +4,29 @@ export default Ember.Component.extend({
     infoMovie: [],
     trailer: "",
     result: "",
+    loading: '',
     actions: {
         getMovie(url, name) {
           var self = this;
+          this.set('loading','loader');
           Ember.$.get('http://localhost:8080/getMovieInfo?url='+url+'&dom=www.mejortorrent.com').then(function(data){
             self.set('infoMovie',data);
+            self.set('loading','');
           });
           var nombre = name;
           nombre = nombre.replace(' ','%20');
-          console.log(nombre);
+          this.set('loading','loader');
           Ember.$.get('http://localhost:8080/getTrailer?name='+nombre).then(function(data){
             self.set('trailer',data);
+            self.set('loading','');
           });
         },
         sendMovie(url) {
             var self = this;
+            this.set('loading','loader');
             Ember.$.get('http://localhost:8080/sendMovie?address='+url+'&ip=192.168.1.34&dom=www.mejortorrent.com').then(function(data){
               self.set('result',data);
-              console.log(data);
+              self.set('loading','');
             });
           }
       }
--- app/controllers/library.js
+++ app/controllers/library.js
@@ -2,13 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
     search: true,
+    loading: "",
     movies: [],
     actions: {
         buscar(busqueda){
             var self = this;
+            this.set('loading','loader');
             Ember.$.getJSON('http://localhost:8080/searchMovie?name='+busqueda+'&dom=www.mejortorrent.com').then(function(data){
                var movies = [];
                movies = data;
+               self.set('loading','');
             if (movies.length == 0) {
                     self.set('search', false);
                 } else {
